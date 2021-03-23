528f554342941e9232b59b13def36603d9ea6028
Muthukumaran_M <muthukumaran.muthiah@qedtek.com>
2021-03-23 15:21:43 +0530

    Experience issue.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  yearsofExperience:string;
+
   constructor() { }
 
   ngOnInit(): void {
+    let exp=(new Date().getMonth() -
+    new Date(2018, 11, 29).getMonth() +
+        12 * (new Date().getFullYear() - new Date(2018, 11, 29).getFullYear())) /
+        12;
+    this.yearsofExperience=parseFloat(<string><unknown>exp).toFixed(1);
   }
 
 }
